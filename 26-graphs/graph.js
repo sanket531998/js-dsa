6afaaf7cf69f0c3f506b32a83eea9888540c883c
sanket_35 <sanket.naukarkar@gmail.com>
2025-03-18 14:01:13 +0530

    graph

--- 26-graphs/graph.js
+++ 26-graphs/graph.js
@@ -9,4 +9,26 @@ class Graph {
     // this if checks to avoid duplication
     if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
   }
+
+  addEdge(v1, v2) {
+    this.adjacencyList[v1].push([v2]);
+    this.adjacencyList[v2].push([v1]);
+  }
+
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      (v) => v !== vertex2
+    );
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      (v) => v !== vertex1
+    );
+  }
+
+  removeVertex(vertex) {
+    while (this.adjacencyList[vertex].length) {
+      let adjacentVertex = this.adjacencyList[vertex].pop();
+      this.removeVertex(vertex, adjacentVertex);
+    }
+    delete this.adjacencyList[vertex];
+  }
 }
